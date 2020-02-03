12789c8ff5301e56153384729289ea4fc0deb1e6
Jady <felixouma379@gmail.com>
2020-02-03 14:50:00 +0300

    import urllib.request and json in request.py file

---
+++ app/models/source.py
@@ -0,0 +1,12 @@
+class Source:
+    '''
+    Source class to define Source Objects
+    '''
+
+
+    def __init__(self,id,name,description,language,country):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.language = language
+        self.country = country
