05809ac817b7783c6d4d41e068b6cf9c3290caee
Jady <felixouma379@gmail.com>
2020-02-05 15:20:52 +0300

    deployment to heroku

--- manage.py
+++ manage.py
@@ -1,4 +1,13 @@
-from app import app
+manager = Manager(app)
+manager.add_command('server',Server)
 
-if __name__ == "__main__":
-    app.run()
+manager.add_command('server',Server)
+@manager.command
+def test():
+""" Run the unittest."""
+    import unittest
+    tests=unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+if __name__== '__main__':
+    manager.run()
---
+++ start.sh
@@ -0,0 +1,1 @@
+export NEWS_API_KEY = 'cb11bc0bcb56433db9af73d42788be5d'
